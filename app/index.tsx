e344d1b3697e293af4d181161d4a9e5b370c0920
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-01-24 21:11:36 +0200

    add navigations and use the useRouter Hook

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  Alert,
-  PixelRatio,
-  Pressable,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import { theme } from "./theme";
-import { ShoppingListItem } from "./components/ShoppingListItem";
+import { Alert, PixelRatio, Pressable, StyleSheet, View } from "react-native";
+import { theme } from "../theme";
+import { ShoppingListItem } from "../components/ShoppingListItem";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Link
+        href="/counter"
+        style={{ textAlign: "center", fontSize: 24, marginBottom: 18 }}
+      >
+        Go To Counter Page
+      </Link>
       <ShoppingListItem name="Coffee" isCompleted />
       <ShoppingListItem name="Tea" />
       <ShoppingListItem name="Sugar" />
