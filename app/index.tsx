227cb089e2af97039ea7604c08cf8647ac327532
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-01-25 13:07:01 +0200

    add the new neded ingrediant

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,49 @@
 import { StatusBar } from "expo-status-bar";
-import { Alert, PixelRatio, Pressable, StyleSheet, View } from "react-native";
+import {
+  Alert,
+  PixelRatio,
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import { theme } from "../theme";
 import { ShoppingListItem } from "../components/ShoppingListItem";
 import { Link } from "expo-router";
+import { useState } from "react";
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "Coffe" },
+  { id: "2", name: "Tea" },
+  { id: "3", name: "Milk" },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] =
+    useState<ShoppingListItemType[]>(initialList);
+
+  const [val, setVal] = useState("");
+
+  function handleInputs(inputText: string) {
+    setVal(inputText);
+    console.log(inputText);
+  }
+
+  function handleSubmit() {
+    const newShoppingList = [
+      { id: new Date().toTimeString(), name: val },
+      ...shoppingList,
+    ];
+    setShoppingList(newShoppingList);
+    setVal("");
+  }
+
   return (
     <View style={styles.container}>
       {/* <Link
@@ -13,9 +52,21 @@ export default function App() {
       >
         Go To Counter Page
       </Link> */}
-      <ShoppingListItem name="Coffee" isCompleted />
-      <ShoppingListItem name="Tea" />
-      <ShoppingListItem name="Sugar" />
+      <View style={styles.inputContaienr}>
+        <Text style={styles.label}>the Main input</Text>
+        <TextInput
+          style={styles.textInput}
+          value={val}
+          onChangeText={handleInputs}
+          returnKeyType="done"
+          onSubmitEditing={handleSubmit}
+        />
+      </View>
+      {shoppingList.map((item) => (
+        <ShoppingListItem name={item.name} key={item.id} />
+      ))}
+      {/* <ShoppingListItem name="Tea" />
+      <ShoppingListItem name="Sugar" /> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -26,6 +77,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.colorWhite,
     // alignItems: "center",
-    justifyContent: "center",
+    // justifyContent: "center",
+  },
+  inputContaienr: {
+    marginHorizontal: 12,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 12,
+    marginBottom: 10,
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: theme.colorLightGrey,
+    padding: 12,
+    borderRadius: 50,
+    fontSize: 18,
   },
 });
