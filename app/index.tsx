f65130f5cf5498580a2db4d8592daf5ec368b2dc
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-01-25 14:16:05 +0200

    add more attributes and styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import {
   Alert,
+  FlatList,
   PixelRatio,
   Pressable,
   ScrollView,
@@ -20,9 +21,9 @@ type ShoppingListItemType = {
 };
 
 const initialList: ShoppingListItemType[] = [
-  { id: "1", name: "Coffe" },
-  { id: "2", name: "Tea" },
-  { id: "3", name: "Milk" },
+  // { id: "1", name: "Coffe" },
+  // { id: "2", name: "Tea" },
+  // { id: "3", name: "Milk" },
 ];
 
 export default function App() {
@@ -48,31 +49,58 @@ export default function App() {
   }
 
   return (
-    <ScrollView style={styles.container} stickyHeaderIndices={[0]}>
-      {/* <Link
-        href="/counter"
-        style={{ textAlign: "center", fontSize: 24, marginBottom: 18 }}
-      >
-        Go To Counter Page
-      </Link> */}
-      <View style={styles.inputContaienr}>
-        {/* <Text style={styles.label}>the Main input</Text> */}
-        <TextInput
-          placeholder="E.g. Coffee"
-          style={styles.textInput}
-          value={val}
-          onChangeText={handleInputs}
-          returnKeyType="done"
-          onSubmitEditing={handleSubmit}
-        />
-      </View>
-      {shoppingList.map((item) => (
-        <ShoppingListItem name={item.name} key={item.id} />
-      ))}
-      {/* <ShoppingListItem name="Tea" />
-      <ShoppingListItem name="Sugar" /> */}
-      <StatusBar style="auto" />
-    </ScrollView>
+    <FlatList
+      style={styles.container}
+      stickyHeaderIndices={[0]}
+      data={shoppingList}
+      ListEmptyComponent={() => {
+        return (
+          <View style={styles.listEmptyContainer}>
+            <Text>Your shopping list is empty!</Text>
+          </View>
+        );
+      }}
+      ListHeaderComponent={
+        <View style={styles.inputContaienr}>
+          <TextInput
+            placeholder="E.g. Coffee"
+            style={styles.textInput}
+            value={val}
+            onChangeText={handleInputs}
+            returnKeyType="done"
+            onSubmitEditing={handleSubmit}
+          />
+        </View>
+      }
+      renderItem={({ item }) => {
+        return <ShoppingListItem name={item.name} />;
+      }}
+    />
+    // <ScrollView style={styles.container} stickyHeaderIndices={[0]}>
+    //   {/* <Link
+    //     href="/counter"
+    //     style={{ textAlign: "center", fontSize: 24, marginBottom: 18 }}
+    //   >
+    //     Go To Counter Page
+    //   </Link> */}
+    //   <View style={styles.inputContaienr}>
+    //     {/* <Text style={styles.label}>the Main input</Text> */}
+    //     <TextInput
+    //       placeholder="E.g. Coffee"
+    //       style={styles.textInput}
+    //       value={val}
+    //       onChangeText={handleInputs}
+    //       returnKeyType="done"
+    //       onSubmitEditing={handleSubmit}
+    //     />
+    //   </View>
+    //   {shoppingList.map((item) => (
+    //     <ShoppingListItem name={item.name} key={item.id} />
+    //   ))}
+    //   {/* <ShoppingListItem name="Tea" />
+    //   <ShoppingListItem name="Sugar" /> */}
+    //   <StatusBar style="auto" />
+    // </ScrollView>
   );
 }
 
@@ -100,5 +128,11 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     fontSize: 18,
     backgroundColor: theme.colorWhite,
+  },
+  listEmptyContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 20,
   },
 });
