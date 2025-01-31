cc3c6b42c8681a21086b9e64c5854a64a902253e
Tarek Abdo <tarekk.abdo22@gmail.com>
2025-01-31 22:06:01 +0200

    push notifications

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,17 +1,24 @@
 import { useRouter } from "expo-router";
 import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { theme } from "../../theme";
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 
 export default function CounterScreen() {
   //   const router = useRouter();
+
+  const handleRequestPermission = async () => {
+    const result = await registerForPushNotificationsAsync();
+    console.log(result);
+  };
   return (
     <View style={styles.container}>
-      {/* <TouchableOpacity onPress={() => router.navigate("/idea")}>
-        <Text style={{ textAlign: "center", fontSize: 24, marginBottom: 18 }}>
-          Go To Idea
-        </Text>
-      </TouchableOpacity> */}
-
-      <Text style={styles.text}>Counter</Text>
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        activeOpacity={0.8}
+        onPress={handleRequestPermission}
+      >
+        <Text style={styles.buttonText}>Request Permission</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -23,7 +30,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#fff",
   },
-  text: {
-    fontSize: 24,
+  buttonContainer: {
+    backgroundColor: theme.ColorBalck,
+    borderRadius: 6,
+    padding: 12,
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: theme.colorWhite,
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
